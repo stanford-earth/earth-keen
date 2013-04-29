85bd69223a7aefc43dbf5099160ab2bedb3f3f2e
Aaron Cole <aaronc@stanford.edu>
2013-04-29 15:36:50 -0700

    Reveting back to using a page template to hide homepge content.

--- region--content.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php if (!empty($content)): ?>
-<div class="<?php print $classes; ?> clearfix">
-  <?php
-  // Only print content if not homepage
-  if (!drupal_is_front_page()) { print $content; }
-  ?>
-</div>
-<?php endif; ?> <!-- /.region -->
