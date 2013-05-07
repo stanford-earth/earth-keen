fc70ae8dae67fd2fee217dce86f6e0bb28f87d7a
Aaron Cole <aaronc@stanford.edu>
2013-05-07 14:42:42 -0700

    Adding display suite templates.

---
+++ ds_layouts/grid/grid.tpl.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file
+ * Display Suite Grid template.
+ *
+ * Available variables:
+ *
+ * Layout:
+ * - $classes: String of classes that can be used to style this layout.
+ * - $contextual_links: Renderable array of contextual links.
+ * - $layout_wrapper: wrapper surrounding the layout.
+ *
+ * Regions:
+ *
+ * - $media: Rendered content for the "Media" region.
+ * - $media_classes: String of classes that can be used to style the "Media" region.
+ *
+ * - $text: Rendered content for the "Text" region.
+ * - $text_classes: String of classes that can be used to style the "Text" region.
+ */
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
+
+  <!-- Needed to activate contextual links -->
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+    <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+    <<?php print $media_wrapper; ?> class="ds-media<?php print $media_classes; ?>">
+      <?php print $media; ?>
+    </<?php print $media_wrapper; ?>>
+
+    <<?php print $text_wrapper; ?> class="ds-text<?php print $text_classes; ?>">
+      <?php print $text; ?>
+    </<?php print $text_wrapper; ?>>
+
+</<?php print $layout_wrapper ?>>
+
+<!-- Needed to activate display suite support on forms -->
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
