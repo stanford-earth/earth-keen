2cc98b43b1e8e071864b2281575485e9fcd704a8
Aaron Cole <aaronc@sesstage.stanford.edu>
2013-11-05 10:29:20 -0800

    Removed watchdog

--- template.php
+++ template.php
@@ -2,7 +2,7 @@
 function earth_keen_preprocess_page(&$vars) {
   // theme setting for masthead image
   $masthead = theme_get_setting('masthead');
-  watchdog('notice','masthead = ' . $masthead);
+  //watchdog('notice','masthead = ' . $masthead);
   $masthead = file_create_url(file_load($masthead)->uri);
   drupal_add_css('#header {background: pink url(' . $masthead  . ') no-repeat 0 0;}', array('group' => CSS_THEME, 'type' => 'inline'));
 }
