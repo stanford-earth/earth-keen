fb8f636e81babb1c0376a7bcd6cd651030e5a697
Aaron Cole <aaronc@sesstage.stanford.edu>
2013-11-05 10:08:22 -0800

    Passing the masthead variable to the CSS.

--- template.php
+++ template.php
@@ -2,8 +2,7 @@
 function earth_keen_preprocess_page(&$vars) {
   // theme setting for masthead image
   $masthead = theme_get_setting('masthead');
-  $masthead = 'green';
-  watchdog('notice',print_r($masthead,true));
-  //$image_url = file_create_url(file_load($masthead)->uri);
-  drupal_add_css('#header {background: transparent url(' . $masthead  . ') no-repeat 0 0;}', array('group' => CSS_THEME, 'type' => 'inline'));
+  watchdog('notice','masthead = ' . $masthead);
+  $masthead = file_create_url(file_load($masthead)->uri);
+  drupal_add_css('#header {background: pink url(' . $masthead  . ') no-repeat 0 0;}', array('group' => CSS_THEME, 'type' => 'inline'));
 }
