ecf472558c279a7816b51328bd83138bfee53140
Aaron Cole <aaronc@sesstage.stanford.edu>
2013-11-05 10:09:05 -0800

    Adding submit handler.

--- theme-settings.php
+++ theme-settings.php
@@ -11,12 +11,19 @@ function earth_keen_form_system_theme_settings_alter(&$form, &$form_state) {
         'file_validate_extensions' => array('gif png jpg jpeg'),
       ),
     );
+
+    // Attach submit handler to the form
     $form['#submit'][] = 'masthead_upload_submit';
     $form['#validate'][] = 'masthead_upload_validate';
 }
 
 function masthead_upload_submit($form, &$form_state) {
   watchdog('notice','masthead_upload_submit');
+  $file = file_save_upload('masthead');
+  $fid = $file->fid;
+  //Set the status of the uploaded file.
+  $file->status = FILE_STATUS_PERMANENT;
+  file_save($file);
 }
   
 function masthead_upload_validate($form, &$form_state) {
