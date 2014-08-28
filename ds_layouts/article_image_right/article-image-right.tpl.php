313181903671ccddb305324b13cb3916dcc3bb6b
Aaron Cole <aaronc@stanford.edu>
2014-08-28 10:18:20 -0700

    Updating layout.

--- ds_layouts/article_image_right/article-image-right.tpl.php
+++ ds_layouts/article_image_right/article-image-right.tpl.php
@@ -27,12 +27,18 @@
   <?php endif; ?>
 
     <div class="article">
-    <<?php print $text_wrapper; ?> class="ds-text span7<?php print $text_classes; ?>">
-      <?php print $text; ?>
-    </<?php print $text_wrapper; ?>>
-    <<?php print $image_wrapper; ?> class="ds-image figure span5 <?php print $image_classes; ?>">
+    <<?php print $header_wrapper; ?> class="ds-text<?php print $header_classes; ?>">
+      <?php print $header	; ?>
+    </<?php print $header_wrapper; ?>>
+    <<?php print $image_wrapper; ?> class="span5 pull-right <?php print $image_classes; ?>">
       <?php print $image; ?>
     </<?php print $image_wrapper; ?>>
+    <<?php print $text_wrapper; ?> class="span6 <?php print $text_classes; ?>">
+      <?php print $text; ?>
+    </<?php print $text_wrapper; ?>>
+    <<?php print $footer_wrapper; ?> class="<?php print $footer_classes; ?>">
+      <?php print $footer; ?>
+    </<?php print $footer_wrapper; ?>>
 
     </div>
 
