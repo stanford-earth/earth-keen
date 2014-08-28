6c08eddc9456da454d99634ed55bc9caad856591
Aaron Cole <aaronc@stanford.edu>
2014-08-28 14:44:04 -0700

    Hacking in a style to override left-margin on first span

--- ds_layouts/article_image_right/article-image-right.tpl.php
+++ ds_layouts/article_image_right/article-image-right.tpl.php
@@ -33,7 +33,7 @@
     <<?php print $image_wrapper; ?> class="span5 pull-right <?php print $image_classes; ?>">
       <?php print $image; ?>
     </<?php print $image_wrapper; ?>>
-    <<?php print $text_wrapper; ?> class="span6 <?php print $text_classes; ?>">
+    <<?php print $text_wrapper; ?> class="span6 <?php print $text_classes; ?>" style="margin-left: 0;">
       <?php print $text; ?>
     </<?php print $text_wrapper; ?>>
     <<?php print $footer_wrapper; ?> class="<?php print $footer_classes; ?>">
